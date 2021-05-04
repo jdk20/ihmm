2886325c36ec6903f899c55107985e43ab6b6f4d
Jon Kenny <jkenny@caltech.edu>
2021-05-04 15:39:17 -0700

    Fixed indexing on emission generation

--- internal/genemission.m
+++ internal/genemission.m
@@ -1,11 +1,11 @@
-function [y,m,m_oracle] = genemission(s,beta_emission,gamma_emission,verbose)
-%GENEMISSION Infinite HMM emission transition generative mechanism.
-%   S = GENEMISSION(T,A,B,Y) returns a sequence of hidden states generated using the
-%   scalar hyperparameters A, B, Y, which are >= 0. T is the number of iterations
-%   and must be >= 1.
+function [y,m,mo] = genemission(s,b_e,y_e,verbose)
+%GENEMISSION Infinite HMM emission generative mechanism.
+%   Y = GENEMISSION(S,B_E,Y_E) returns emissions generated using the the hidden
+%   states in S. Generation uses the scalar hyperparameters B_E and Y_E, which 
+%   must be >= 0.
 %
-%   [S,N,NO] = GENEMISSION(T,A,B,Y) returns the count of transitions between 
-%   hidden states, and the count of oracle-mediated transitions.
+%   [Y,M,MO] = GENEMISSION(S,B_E,Y_E) returns the count of emissions from 
+%   hidden states, and the count of oracle-mediated emissions.
 %
 %   References:
 %      [1] M. Beal, et. al., "The Infinite Hidden Markov Model", 2002
@@ -21,30 +21,27 @@ else
     end
 end
 
-if ~isscalar(beta_emission) || beta_emission < 0 || ...
-        ~isscalar(gamma_emission) || gamma_emission < 0
+if ~isscalar(b_e) || b_e < 0 || ...
+        ~isscalar(y_e) || y_e < 0
     error('Hyperparameters must be scalars and >=0.');
 end
 
 T = length(s);
 K = max(s);
-
 y = NaN(T, 1); % Output sequence of emissions
-y(1) = 1;
 
-Q = 1; % Initial number of unique emissions
+Q = 0; % Initial number of unique emissions
 m = zeros(K, Q); % Number of transitions between states (non-oracle)
-m_oracle = 1; % Number of transitions from oracle to each state
+mo = zeros(1, Q); % Number of transitions from oracle to each state
 for t = 1:T
     i = s(t);
-    q = y(t);
     
     % (non-oracle) DP
     p = NaN(Q + 1, 1);
     for q = 1:Q
-        p(q) = m(i,q)/(sum(m(i,:)) + beta_emission); % Existing
+        p(q) = m(i,q)/(sum(m(i,:)) + b_e); % Existing
     end
-    p(Q+1) = beta_emission/(sum(m(i,:)) + beta_emission); % Oracle
+    p(Q+1) = b_e/(sum(m(i,:)) + b_e); % Oracle
     
     % Random draw for non-oracle DP
     idx = randsample(1:length(p), 1, true, p);
@@ -52,9 +49,9 @@ for t = 1:T
     if idx == (Q + 1) % Oracle DP
         p = NaN(Q + 1, 1);
         for q = 1:Q
-            p(q) = m_oracle(q)/(sum(m_oracle) + gamma_emission);
+            p(q) = mo(q)/(sum(mo) + y_e);
         end
-        p(Q+1) = gamma_emission/(sum(m_oracle) + gamma_emission);
+        p(Q+1) = y_e/(sum(mo) + y_e);
 
         % Random draw for oracle DP
         idx = randsample(1:length(p), 1, true, p);
@@ -64,35 +61,39 @@ for t = 1:T
             temp_m = zeros(K, Q + 1);
             temp_m_oracle = zeros(1, Q + 1);
             temp_m(1:K, 1:Q) = m;
-            temp_m_oracle(1:Q) = m_oracle;
+            temp_m_oracle(1:Q) = mo;
             m = temp_m;
-            m_oracle = temp_m_oracle;
+            mo = temp_m_oracle;
             
             m(i, idx) = m(i, idx) + 1;
-            m_oracle(idx) = m_oracle(idx) + 1;
+            mo(idx) = mo(idx) + 1;
             
-            y(t+1) = Q + 1;
+            y(t) = Q + 1;
             Q = Q + 1;
             
             if verbose
-                disp(['(t=',num2str(t+1),') Oracle new emission ',num2str(Q),' (prob: ', num2str(100*p(idx)),')']);
+                disp(['(t=',num2str(t),') Oracle new emission ',num2str(Q), ...
+                    ' (prob: ', num2str(100*p(idx)),')']);
             end
         else
-            y(t+1) = idx;
+            y(t) = idx;
             m(i, idx) = m(i, idx) + 1;
-            m_oracle(idx) = m_oracle(idx) + 1;       
+            mo(idx) = mo(idx) + 1;       
             
             if verbose
-                disp(['(t=',num2str(t+1),') Oracle state ',num2str(i),' emission ',num2str(idx),' (prob: ', num2str(100*p(idx)),')']);
+                disp(['(t=',num2str(t),') Oracle state ',num2str(i), ...
+                    ' emission ',num2str(idx), ...
+                    ' (prob: ', num2str(100*p(idx)),')']);
             end
         end
  
     else % Self/existing transition
-        y(t+1) = idx;
+        y(t) = idx;
         m(i, idx) = m(i, idx) + 1;
         
         if verbose
-            disp(['(t=',num2str(t+1),') State ',num2str(i),' emission ',num2str(idx),' (prob: ', num2str(100*p(idx)),')']);
+            disp(['(t=',num2str(t),') State ',num2str(i),' emission ', ...
+                num2str(idx),' (prob: ', num2str(100*p(idx)),')']);
         end
     end
 end
